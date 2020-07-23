4c007b48f119f50f0b6485c8eec6ed27fd50a0dc
Daniel Yang <yangafc@bc.edu>
2020-07-22 20:28:10 -0400

    Initial Commit

---
+++ Bonfire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bonfire
+//
+//  Created by Daniel Yang on 7/22/20.
+//  Copyright © 2020 Daniel Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
